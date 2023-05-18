6fe42d52c3604728bd0972979c771106243029ae
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-05-18 07:15:05 +0200

    1

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import css from './Statistic.module.css';
 
 export const Notification = ({ message }) => {
+  console.log('hello');
   return (
     <>
       <p className={css.feedback}>{message}</p>
